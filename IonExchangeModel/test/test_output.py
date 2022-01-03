51779c518641620c3d64087afa71422a5a8dc98f
Levi Haupert <49239747+LeviHaupert@users.noreply.github.com>
2022-01-03 08:10:28 -0500

    Update output test for new pandas excel writer

--- IonExchangeModel/test/test_output.py
+++ IonExchangeModel/test/test_output.py
@@ -36,21 +36,21 @@ class OutputTestSuite(unittest.TestCase):
 
     def test_out_units_mg(self):
 
-        ions = pd.read_excel(self.infile, sheet_name='ions', index=0)
-        Cin = pd.read_excel(self.infile, sheet_name='Cin', index=0)        
-        params = pd.read_excel(self.infile, sheet_name='params', index=0)
+        ions = pd.read_excel(self.infile, sheet_name='ions', index_col=0)
+        Cin = pd.read_excel(self.infile, sheet_name='Cin', index_col=0)        
+        params = pd.read_excel(self.infile, sheet_name='params', index_col=0)
         
-        ions.loc[0, 'units'] = 'mg'
-        Cin.iloc[:, 1] = Cin.iloc[:, 1] * ions.loc[0, 'mw']
+        ions.loc['CHLORIDE', 'units'] = 'mg'
+        Cin.iloc[:, 0] = Cin.iloc[:, 0] * ions.loc['CHLORIDE', 'mw']
         
         mock_infile = io.BytesIO()
         writer = pd.ExcelWriter(mock_infile, engine='xlsxwriter')
-        params.to_excel(writer, sheet_name='params', index=0)
-        ions.to_excel(writer, sheet_name='ions', index=0)
-        Cin.to_excel(writer, sheet_name='Cin', index=0)    
+        params.to_excel(writer, sheet_name='params')
+        ions.to_excel(writer, sheet_name='ions')
+        Cin.to_excel(writer, sheet_name='Cin')    
         writer.save()
         
-        IEX = hsdmix.HSDMIX(writer) 
+        IEX = hsdmix.HSDMIX(writer.handles.handle) 
        
         writer.close()
         
@@ -63,9 +63,9 @@ class OutputTestSuite(unittest.TestCase):
         mock_outfile = io.BytesIO()
         IEX.save_results(mock_outfile)
         
-        result_df = pd.read_excel(mock_outfile, index=0)
-        Cl_out = result_df.values[-1, 1]
-                
+        result_df = pd.read_excel(mock_outfile, index_col=0)
+        Cl_out = result_df.values[-1, 0]
+      
         assert np.allclose(Cl_out, 53.52952053)
         
 
