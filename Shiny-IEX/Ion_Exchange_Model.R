862d779cb592500cfc925fc231b6489bdfdf839d
colesandlin <65195437+colesandlin@users.noreply.github.com>
2024-09-27 08:45:04 -0400

    Add toggle for units

--- Shiny-IEX/Ion_Exchange_Model.R
+++ Shiny-IEX/Ion_Exchange_Model.R
@@ -1893,8 +1893,11 @@ server <- function(input, output, session) {
   
   observe({
     toggleState("Vv", condition=input$veloselect!="Volumetric")
+    toggleState("VelocityUnits", condition = input$veloselect != "Volumetric") # Velocity units are grayed out if velocity is not being used
     toggleState("Fv", condition=input$veloselect!="Linear")
+    toggleState("FlowrateUnits", condition = input$veloselect != "Linear") # Flowrate units are grayed out if flowrate is not being used
     toggleState("Dv", condition=input$veloselect!="Linear")
+    toggleState("DiameterUnits", condition = input$veloselect != "Linear") # Diameter units are grayed out if diameter is not being used
   })
   
   velocityvar<-reactiveVal()
