correcting Jacobian HSDMIX

IonExchangeModel/ixpy/hsdmix.py
@@ -55,12 +55,7 @@ def approx_Jac_struc(nr, NION, nz):
     nz: Number of axial collocation points
     ...
     Because the order of the concentration array is (nr, NION, nz),
-    and the order of r is [C, q_r=0, ..., q_r=rb], the ion exchange zones
-    will be in the (NION*nz x NION*nz) corners of the Jacobian.
-    They have band structure, but we will assume they are dense for simplicity.
-    ...
-    The Jacobian is dominated by a sparse banded structure from diffusion in the
-    solid phase. There are 2*nr - 1 diagonals.
+    and the order of r is [C, q_r=0, ..., q_r=rb].
     ...
     Returns Jac_struc: an array of ones and zeros.
     """
@@ -69,17 +64,23 @@ def approx_Jac_struc(nr, NION, nz):
     
     Jac_struc = np.zeros((NEQ, NEQ))
     
-    # Diffusion zone
+    # Diffusion/exchange zone
     Jac_struc[nzni:, nzni:] += np.eye(NEQ-nzni, NEQ-nzni, k=0)
-    for ii in range(1, nr):
-        Jac_struc[nzni:, nzni:] += np.eye(NEQ-nzni, NEQ-nzni, k=(ii*nzni))
-        Jac_struc[nzni:, nzni:] += np.eye(NEQ-nzni, NEQ-nzni, k=-(ii*nzni))
+    
+    # No axial interactions in beads
+    for ii in range(NION*(1+nr)-1):
+        ii = ii+1
+      
+        # just do the whole block and zero out later. It's easier.
+        Jac_struc[:, :] += np.eye(NEQ, NEQ, k=nz*ii)
+        Jac_struc[:, :] += np.eye(NEQ, NEQ, k=-nz*ii)
+    
+    # zero out liquid phase
+    Jac_struc[0:nzni,:] = 0.0        
     
     # Block off corners (ion exchange zones)
-    Jac_struc[0:nzni, 0:nzni] = 1.0
-    Jac_struc[0:nzni, (nr)*nzni:] = 1.0
-    Jac_struc[(nr)*nzni:, 0:nzni] = 1.0
-    Jac_struc[(nr)*nzni:, (nr)*nzni:] = 1.0
+    Jac_struc[0:nzni, 0:nzni] = 1.0 # advection zones
+    Jac_struc[0:nzni, (nr)*nzni:] = 1.0 # bead surface
     
     return Jac_struc
 
@@ -302,8 +303,8 @@ class HSDMIX:
         v = np.float64(self.params['v']) # linear flow velocity (cm/s)
         nz = int(self.params['nz']) # number of axial collocation points
         nr = int(self.params['nr']) # number of radial collocation points
-        Ds = np.float64(self.params['Ds']) # overall resin phase diffusion coefficient (cm**2/s)
-        kL =  np.float64(self.params['kL'])   # overall film transfer coefficient (cm/s)
+        # Ds = np.float64(self.params['Ds']) # overall resin phase diffusion coefficient (cm**2/s)
+        # kL =  np.float64(self.params['kL'])   # overall film transfer coefficient (cm/s)
         rb = np.float64(self.params['rb']) # resin radius (cm)
         Q = np.float64(self.params['Q']) # Resin capacity by volume (meq/L)
 
@@ -511,7 +512,7 @@ class HSDMIX:
                 dq_dT[:, iii, :] =  Ds_iii * t_half / rb**2 * Br_q[:, iii, :]   
 
             # # explicitly doing implicit chloride LMH
-            # dq_dT[:, 0, :] = -dq_dT[:, 1:, :].sum(axis=1) # XXX: Why doesn't work?
+            dq_dT[:, 0, :] = -dq_dT[:, 1:, :].sum(axis=1) # XXX: Why doesn't work?
             # print(dq_dT[-2, :, -1].sum()) # Why isn't that zero?
 
             # intermediate term for dq_dT at bead surface      HSDMIX   